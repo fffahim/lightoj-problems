ac022234c6c127e83da29035dddcabddce6c8992
Belal Mahmud Fahim <belal.mahmud099@gmail.com>
2021-01-22 00:38:27 +0600

    Consistency Checker/Trie

--- lightoj_1254.cpp
+++ lightoj_1254.cpp
@@ -1,100 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
-int arr[550],n,m,s;
-vector<int>vec[550],cost[550];
-int dp[550][20],check[550][550];
-struct node{
-    int u,w,ghursi;
-    node(int _u,int _w,int _ghursi)
-    {
-        u=_u;
-        w=_w;
-        ghursi=_ghursi;
-    }
-    bool operator < ( const node& p ) const {      return w > p.w;   }
-};
-void djkstra()
+int n;
+vector<int>vec[1000005];
+int mark[1000005];
+int dfs(int i)
 {
-    for(int i=0;i<=n;i++)
-    {
-        for(int j=0;j<=15;j++) dp[i][j]=1e8+5;
-    }
-    priority_queue<node>p;
-    if(arr[0]==1)
-    {
-        dp[0][1]=0;
-        p.push(node(0,0,1));
-    }
-    else
-    {
-        dp[0][0]=0;
-        p.push(node(0,0,0));
-    }
-    while(!p.empty())
-    {
-        int u=p.top().u;
-        int cos=p.top().w;
-        int ghursi=p.top().ghursi;
-        p.pop();
-        for(int i=0;i<vec[u].size();i++)
-        {
-            int v= vec[u][i];
-            if(check[u][v]==-1)
-            {
-                check[u][v]=0;
-                int vw= cost[u][i]+cos;
-                int vghursi= ghursi+arr[v];
-                if(vghursi>s) vghursi=s;
-                if(dp[v][vghursi]> vw )
-                {
-                    dp[v][vghursi]=vw;
-                    p.push(node(v,vw,vghursi));
-                }
-            }
-        }    
-    }
+	for(int x=0;x<=n;x++)
+	{
+		mark[x]=0;
+	}
+	deque<int>de;
+	de.push_back(i);
+	mark[i]=1;
+	int sum=0;
+	while(!de.empty())
+	{
+		int temp= de.front();
+		de.pop_front();
+		int par= mark[temp];
+		for(int j=0;j<vec[temp].size();j++)
+		{
+			if(mark[vec[temp][j]]==0)
+			{
+				mark[vec[temp][j]]=par+1;
+				sum=max(sum,mark[vec[temp][j]]);
+				de.push_back(vec[temp][j]);
+			}
+		}
+	}
+	return sum;
 }
 int main()
 {
-    //  freopen("hacki.txt","r",stdin);
-    // freopen("hacko.txt","w",stdout);
-    int t;
-    scanf("%d",&t);
-    for(int k=1;k<=t;k++)
-    {
-        scanf("%d %d %d", &n, &m, &s);
-        for(int i=0;i<=n;i++)
-        {
-            vec[i].clear();
-            cost[i].clear();
-            arr[i]=0;
-            for(int j=0;j<=n;j++) check[i][j]=-1;
-        }
-        for(int i=0;i<s;i++)
-        {
-            int x;
-            cin>>x;
-            arr[x]=1;
-        }
-        for(int i=0;i<m;i++)
-        {
-            int u,v,w;
-            cin>>u>>v>>w;
-            vec[u].push_back(v);
-            cost[u].push_back(w);
-        }
-        djkstra();
-        int flag=0;
-        cout<<"Case "<<k<<": ";
-        for(int i=s;i>=0;i--)
-        {
-            if(dp[n-1][i]!=1e8+5)
-            {
-               cout<<i<<" "<<dp[n-1][i]<<endl;
-               flag=1;
-               break;
-            }
-        }
-        if(flag==0) cout<<"Impossible\n";
-    }
+	scanf("%d", &n);
+	for(int i=0;i<n-1;i++) 
+	{
+		int x,y;
+		scanf("%d %d", &x, &y);
+		vec[x].push_back(y);
+		vec[y].push_back(x);
+	}
+	deque<int>de;
+	for(int i=1;i<=n;i++)
+	{
+		int xx= dfs(i);
+		if(xx==0) xx++;
+		printf("%d\n",xx);
+	}
 }
--- lightoj_1379.cpp
+++ lightoj_1379.cpp
@@ -1,107 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long int dis1[10005],dis2[10005];
-vector<int>vec[10005],rev[10005],cost[10005],cost2[10005];
-int n;
-struct node{
-    int u,w,tot;
-    node(int _u,int _w,int _tot)
-    {
-        u=_u;
-        w=_w;
-        tot=_tot;
-    }
-    bool operator < ( const node& p ) const {      return w > p.w;   }
-};
-void djkstra1(int start)
-{
-    for(int i=1;i<=n;i++)
-    {
-        dis1[i]=5e9+5;
-    }
-    priority_queue<node>p;
-    dis1[start]=0;
-    p.push(node(start,0,0));
-    while(!p.empty())
-    {
-        int u = p.top().u;
-        int tot = p.top().tot;
-        p.pop();
-        for(int i=0;i<vec[u].size();i++)
-        {
-            int v = vec[u][i];
-            int sum= tot + cost[u][i];
-            if(dis1[v]>sum)
-            {
-                dis1[v]=sum;
-                p.push(node(v,cost[u][i],dis1[v]));
-            }
-        }
-    }
-}
-void djkstra2(int start)
-{
-    for(int i=1;i<=n;i++)
-    {
-        dis2[i]=5e9+5;
-    }
-    priority_queue<node>p;
-    dis2[start]=0;
-    p.push(node(start,0,0));
-    while(!p.empty())
-    {
-        int u = p.top().u;
-        int tot = p.top().tot;
-        p.pop();
-        for(int i=0;i<rev[u].size();i++)
-        {
-            int v = rev[u][i];
-            int sum= tot + cost2[u][i];
-            if(dis2[v]>sum)
-            {
-                dis2[v]=sum;
-                p.push(node(v,cost2[u][i],dis2[v]));
-            }
-        }
-    }
-}
 int main()
 {
-    int t;
-    cin>>t;
-    for(int kk=1;kk<=t;kk++)
+    float arr[5],fin;
+    for(int i=0;i<4;i++) cin>>arr[i];
+    cin>>fin;
+    sort(arr,arr+4);
+    float sum=0;
+    float temp= (arr[1]+arr[2]+arr[3])/3.00;
+    float temp2=(arr[0]+arr[1]+arr[2])/3.00;
+    temp = floorf(temp * 100) / 100; 
+    temp2= floorf(temp2 * 100) / 100; 
+    if(temp<=fin)
     {
-        int m,s,t,p;
-        cin>>n>>m>>s>>t>>p;
-        for(int i=0;i<m;i++)
+        cout<<"infinite\n";
+        return 0;
+    }
+    else if(temp2>fin) 
+    {
+        cout<<"impossible\n";
+        return 0;
+    }
+    float index= -1;
+    float check= arr[1]+arr[2];
+    float first= arr[0];
+    float last= arr[3];
+    int i=32;
+    while(i--)
+    {
+        float mid= (last+first)/2.00;
+        temp=(check+mid)/3.00;
+        if(fin-temp>=0.001)
         {
-            int u,v,w;
-            cin>>u>>v>>w;
-            vec[u].push_back(v);
-            cost[u].push_back(w);
-            rev[v].push_back(u);
-            cost2[v].push_back(w);
+            cout<<temp<<" "<<fin<<endl;
+            index=mid;
+            first= mid;
         }
-        djkstra1(s);
-        djkstra2(t);
-        int maxx=-1;
-        for(int i=1;i<=n;i++)
+        else if(fin-temp<0.001)
         {
-            for(int j=0;j<vec[i].size();j++)
-            {
-                int v = vec[i][j];
-                int cos =cost[i][j];
-                if(dis1[i]+cos+dis2[v]<=p) maxx=max(maxx,cos);
-            }
-        }
-        cout<<"Case "<<kk<<": "<<maxx<<endl;
-        for(int i=1;i<=n;i++)
-        {
-            vec[i].clear();
-            rev[i].clear();
-            cost[i].clear();
-            cost2[i].clear();
+            last=mid;
         }
     }
-
+    cout<<index<<endl;
 }
